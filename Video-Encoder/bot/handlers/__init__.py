ae04b2d4a6fbe6fd405afac0cfc0122c7793e271
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-11-04 18:03:52 +0530

    Delete Video-Encoder directory

--- Video-Encoder/bot/handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Handlers package for Video Encoder Bot
-"""
-
-from . import start, encode
-
-# Audio and subtitle handlers can be added later
-__all__ = ['start', 'encode']
--- Video-Encoder/bot/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utilities package for Video Encoder Bot
-"""
-
-from . import ffmpeg_utils, progress_tracker, menu_helpers
-
-__all__ = ['ffmpeg_utils', 'progress_tracker', 'menu_helpers']
